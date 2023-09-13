d07cef158a832a1674a95802172e8c25cae335a1
Konstantin Chukharev <lipen00@gmail.com>
2023-09-13 13:50:52 +0300

    Add example project with Gradle plugin usage

---
+++ byteflow-plugin-usage/build.gradle.kts
@@ -0,0 +1,53 @@
+plugins {
+    kotlin("jvm") version "1.9.10"
+    id("io.github.UnitTestBot.byteflow") version "0.1.0-SNAPSHOT"
+}
+
+buildscript {
+    // Repos for plugin deps:
+    repositories {
+        mavenLocal()
+        mavenCentral()
+        maven("https://jitpack.io")
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+byteflow {
+    configFile = File("../configs/config.json")
+    startClasses = listOf("org.example.NpeExamples")
+    classpath = sourceSets["main"].runtimeClasspath.asPath
+}
+
+tasks.runAnalyzer {
+    dependsOn(tasks.compileJava)
+}
+
+// Debug task
+tasks.register("yeet") {
+    println("-".repeat(42))
+
+    println("project.configurations: {")
+    for ((k, v) in project.configurations.asMap) {
+        println("  $k = $v")
+    }
+    println("}")
+
+    println("project.sourceSets: {")
+    for ((k, v) in project.sourceSets.asMap) {
+        println("  $k = $v")
+    }
+    println("}")
+
+    println("runtimeClasspath = ${sourceSets["main"].runtimeClasspath.asPath}")
+
+    println("-".repeat(42))
+}
+
+tasks.wrapper {
+    gradleVersion = "8.3"
+    distributionType = Wrapper.DistributionType.ALL
+}
---
+++ byteflow-plugin-usage/settings.gradle.kts
@@ -0,0 +1,8 @@
+pluginManagement {
+    repositories {
+        maven {
+            url = uri("../byteflow-gradle/build/repository")
+        }
+        gradlePluginPortal()
+    }
+}
