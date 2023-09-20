2438f582ddda95c8fa9b027b79641d102862279d
Konstantin Chukharev <lipen00@gmail.com>
2023-09-20 21:18:20 +0300

    Put examples back into the main configuration

--- byteflow-core/build.gradle.kts
+++ byteflow-core/build.gradle.kts
@@ -2,18 +2,12 @@ plugins {
     `maven-publish`
 }
 
-sourceSets {
-    create("examples")
-}
-
 dependencies {
     api(Libs.jacodb_api)
     api(Libs.jacodb_core)
     api(Libs.jacodb_analysis)
     implementation(Libs.kotlinx_coroutines_core)
     implementation(Libs.kotlin_logging)
-
-    "examplesCompileOnly"("org.jetbrains:annotations:24.0.1")
 }
 
 publishing {
