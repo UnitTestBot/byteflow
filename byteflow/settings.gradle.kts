d7ff004f0b9741d981df1878b3abedde9766def1
Konstantin Chukharev <lipen00@gmail.com>
2023-09-26 19:53:54 +0300

    Apply gradle-enterprise plugin to the root

--- byteflow/settings.gradle.kts
+++ byteflow/settings.gradle.kts
@@ -1,16 +1,5 @@
 rootProject.name = "byteflow"
 
-plugins {
-    `gradle-enterprise`
-}
-
-gradleEnterprise {
-    buildScan {
-        termsOfServiceUrl = "https://gradle.com/terms-of-service"
-        termsOfServiceAgree = "yes"
-    }
-}
-
 fun myInclude(name: String) {
     include(name)
     project(":$name").projectDir = file("${rootProject.name}-$name")
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,14 @@
 includeBuild("byteflow")
 includeBuild("examples/byteflow-plugin-usage")
 includeBuild("examples/byteflow-juliet")
+
+plugins {
+    `gradle-enterprise`
+}
+
+gradleEnterprise {
+    buildScan {
+        termsOfServiceUrl = "https://gradle.com/terms-of-service"
+        termsOfServiceAgree = "yes"
+    }
+}
